0700285fc27523fd277788faffc3d1a121dd4cac
Lina Espinosa <linaespinosa0823@gmail.com>
2023-01-18 21:52:50 -0500

    Se agrega funcion para dar interactividad al turno correspondiente

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,3 @@
-
 const select_simbols = document.querySelectorAll('.main .table-options .btns')
 const table_game = document.getElementById("table-game")
 const game_cpu = document.getElementById("game_cpu")
@@ -10,49 +9,72 @@ const btn_quit = document.getElementById("quit")
 const btn_next = document.getElementById("next")
 const initGame = document.getElementById(("init"))
 const cellElements = document.querySelectorAll('[data-cell]')
+const playerTurn = document.getElementById("player-turn")
 const x_class = 'equis'
 const circle_class = 'zero'
 const winning_combinations = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ]
-let circleTurn
+let circleTurn = false;
 
 cellElements.forEach(cell => {
-    cell.addEventListener('click', handleClick, { once: true })
+    cell.addEventListener('click', handleClick, {once: true})
 })
 
+
 function handleClick(e) {
-    console.log('first')
     const cell = e.target
     const currentClass = circleTurn ? circle_class : x_class
+    console.log(currentClass)
     placeMark(cell, currentClass)
-    if(checkWin(currentClass)) {
+
+
+    if (checkWin(currentClass)) {
         endGame(false)
     }
     swapTurns()
+
 }
 
-function endGame(draw) {
-    if(draw) {
 
-    }else {
+function endGame(draw) {
+    if (draw) {
+
+    } else {
         takeRound.classList.add('openRound')
     }
 }
 
-function placeMark(cell, currentClass){
+function placeMark(cell, currentClass) {
     cell.classList.add(currentClass)
 }
 
+function playTurn() {
+    const turnEl = document.getElementById('player-turn');
+
+    turnEl.innerHTML = `<svg class="gameplay__turn-icon">
+											<use xlink:href="./assets/images/icon-${
+        circleTurn ? circle_class : x_class
+    }-turn-solid.svg#icon-${
+        circleTurn ? circle_class : x_class
+    }-turn-solid"></use>
+											</svg> &nbsp; TURN`;
+}
+
+// assets/images/icon-equis-solid.svg
+
 function swapTurns() {
     circleTurn = !circleTurn
+    playTurn()
+
+
 }
 
 
@@ -67,6 +89,7 @@ function checkWin(currentClass) {
 btn_quit.addEventListener('click', () => {
     location.reload()
 })
+
 
 load.addEventListener('click', () => {
     takeRound.classList.add('openRound')
